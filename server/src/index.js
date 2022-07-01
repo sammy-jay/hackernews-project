8a2136eddd964b13cf487da1d71a02ae39cff2ac
sammy-jay <sammyjayden27@gmail.com>
2022-07-01 16:11:56 +0000

    added the User and AuthPayload graphql schemas

--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,9 @@ const fs = require("fs");
 const path = require("path");
 
 const prisma = new PrismaClient();
+
 let links = [];
+
 const resolvers = {
   Query: {
     feed: async (parent, args, context) => {
@@ -24,17 +26,6 @@ const resolvers = {
       });
       return newLink;
     },
-    updateLink: (parent, { id, url, description }, context) => {
-        const link = await context.prisma.link.update({
-            where: { id},
-            data: { published: true },
-        })
-        return link
-    },
-    // deleteLink: async (parent, { id }, context ) => {
-    // //   await context.link.
-    //   return null;
-    // },
   },
 };
 
