56746cef83ce011ff00d24507ac0d0f70843aaf9
sammy-jay <sammyjayden27@gmail.com>
2022-07-01 14:59:10 +0000

    added mutation block

--- server/src/index.js
+++ server/src/index.js
@@ -1,19 +1,52 @@
 const { ApolloServer, gql } = require("apollo-server");
+7;
+const fs = require("fs");
+const path = require("path");
 
-const typeDefs = gql`
-  type Query {
-    info: String!
-  }
-`;
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
 
 const resolvers = {
   Query: {
     info: () => "🚀 it works 😀",
+    feed: () => links,
+    link: (parent, { id }) => {
+      return links.find((link) => link.id === id);
+    },
+  },
+  Mutation: {
+    addLink: (parent, { url, description }) => {
+      let idCount = links.length;
+      const newLink = {
+        id: `link-${idCount++}`,
+        description,
+        url,
+      };
+      links.push(newLink);
+      return newLink;
+    },
+    updateLink: (parent, { id, url, description }) => {
+      return links.find((link) => {
+        if (link.id === id) {
+          link.description = description;
+          link.url = url;
+        }
+      });
+    },
+    deleteLink: (parent, { id }) => {
+      links.filter((link) => link.id !== id);
+      return null;
+    },
   },
 };
 
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
   resolvers,
   csrfProtection: true,
   cache: true,
